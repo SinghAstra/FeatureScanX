5c96a30476700e38d29c1531e6f2d50a7e6e7fc3
Astra <abhaypratapsinghwd@gmail.com>
2024-07-03 10:23:29 +0530

    Suggestion Cards

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/Content.css";
 import Posts from "./Posts";
+import Suggestions from "./Suggestions";
 
 const Content = () => {
   return (
     <div className="content">
       <Posts />
+      <Suggestions />
     </div>
   );
 };
