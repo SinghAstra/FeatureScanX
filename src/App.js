c9500f20633f19e08d33209c7e1fcae552be91cb
Astra <abhaypratapsinghwd@gmail.com>
2024-07-10 20:42:11 +0530

    Create New Post Dialog

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
+import CreateNewPost from "./pages/CreateNewPost";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
@@ -19,13 +20,13 @@ function App() {
   const routes = createRoutesFromChildren(
     <>
       <Route element={<PublicRoute />}>
+        <Route path="/" element={<CreateNewPost />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/verify" element={<VerifyOTP />} />
         <Route path="/reset-password" element={<ResetPassword />} />
       </Route>
       <Route element={<PrivateRoute />}>
-        <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
       </Route>
       <Route path="*" element={<NotFound />} />
---
+++ src/pages/CreatePostDialog.js
@@ -0,0 +1,46 @@
+import { faPhotoVideo } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useRef } from "react";
+import "../styles/CreatePostDialog.css";
+const CreatePostDialog = ({ isOpen, onClose }) => {
+  const dialogRef = useRef();
+
+  useEffect(() => {
+    let timeoutId;
+
+    const handleClickOutside = (event) => {
+      if (dialogRef.current && !dialogRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      timeoutId = setTimeout(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+      }, 100); // Add a small delay to avoid immediate close
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen, onClose]);
+  if (!isOpen) return null;
+
+  return (
+    <div className="dialog-overlay">
+      <div className="dialog" ref={dialogRef}>
+        <div className="dialog-header">
+          <h2>Create New Post</h2>
+        </div>
+        <div className="dialog-content">
+          <FontAwesomeIcon icon={faPhotoVideo} size="5x" />
+          <p>Drag Photos and Videos here</p>
+          <button>Select From Computer</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePostDialog;
