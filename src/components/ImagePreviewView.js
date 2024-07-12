a09382229e2df5954f084ca049e7e3da993b5b88
Astra <abhaypratapsinghwd@gmail.com>
2024-07-12 10:35:33 +0530

    Added Zoom Slider

--- src/components/ImagePreviewView.js
+++ src/components/ImagePreviewView.js
@@ -1,16 +1,34 @@
 import { faArrowLeft, faCrop } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef, useState } from "react";
+import { RiZoomInLine } from "react-icons/ri";
 import "../styles/ImagePreviewView.css";
 import CropOptions from "./CropOptions";
 
 const ImagePreviewView = ({ file, onBack, onNext }) => {
   const [showCropOptions, setShowCropOptions] = useState(false);
   const [cropOption, setCropOption] = useState("original");
+  const [showZoomSlider, setShowZoomSlider] = useState(false);
+  const [zoomValue, setZoomValue] = useState(0);
   const cropOptionsRef = useRef(null);
+  const zoomSliderRef = useRef(null);
 
   const handleCropButtonClick = () => {
     setShowCropOptions(!showCropOptions);
+  };
+
+  const handleZoomButtonClick = () => {
+    setShowZoomSlider(!showZoomSlider);
+  };
+
+  const handleZoomChange = (event) => {
+    setZoomValue(event.target.value);
+    console.log("Zoom value:", event.target.value);
+    const percentage =
+      ((event.target.value - event.target.min) /
+        (event.target.max - event.target.min)) *
+      100;
+    zoomSliderRef.current.style.background = `linear-gradient(to right, white ${percentage}%, black ${percentage}%)`;
   };
 
   const handleOptionClick = (option) => {
@@ -53,16 +71,44 @@ const ImagePreviewView = ({ file, onBack, onNext }) => {
             backgroundImage: `url(${file})`,
           }}
         ></div>
+
         <div className="crop-button-container">
-          <div ref={cropOptionsRef}>
-            <CropOptions
-              showCropOptions={showCropOptions}
-              onOptionClick={handleOptionClick}
-              selectedOption={cropOption}
-            />
-          </div>
+          {showCropOptions && (
+            <div ref={cropOptionsRef}>
+              <CropOptions
+                showCropOptions={showCropOptions}
+                onOptionClick={handleOptionClick}
+                selectedOption={cropOption}
+              />
+            </div>
+          )}
           <div onClick={handleCropButtonClick} className="crop-button">
             <FontAwesomeIcon icon={faCrop} />
+          </div>
+        </div>
+        <div className="zoom-button-container">
+          {showZoomSlider && (
+            <div className="zoom-slider-container">
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value={zoomValue}
+                onChange={handleZoomChange}
+                className="zoom-slider"
+                ref={zoomSliderRef}
+              />
+            </div>
+          )}
+          <div onClick={handleZoomButtonClick} className="zoom-button">
+            <RiZoomInLine
+              style={{
+                width: "32px",
+                height: "32px",
+                padding: "5px",
+                fill: "white",
+              }}
+            />
           </div>
         </div>
       </div>
