a8db2c652eec1d355119059b78cf71b73414a8e3
Astra <abhaypratapsinghwd@gmail.com>
2024-07-18 16:57:51 +0530

    views - index.ejs

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 import connectDB from "./database/db.js";
 dotenv.config({ path: "./.env" });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 app.use(cors());
 app.use(morgan("tiny"));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the Social-API-2.0" });
+  res.render("index");
 });
 
 connectDB();
