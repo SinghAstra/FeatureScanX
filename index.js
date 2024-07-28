ef5c5fd8d0f49c0578da790b16dcbd028104a9c6
Astra <abhaypratapsinghwd@gmail.com>
2024-07-28 18:32:37 +0530

    Backend setup

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import helmet from "helmet";
+import mongoose from "mongoose";
 import morgan from "morgan";
+import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
-import connectDB from "./database/db.js";
-dotenv.config({ path: "./.env" });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+dotenv.config();
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.set("view engine", "ejs");
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use(morgan("tiny"));
-app.use(cookieParser());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-connectDB();
-
-app.listen(5000, () => {
-  console.log("Listening on port 5000");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
