c93e91d27817f4bdb70edd780ed92ae1e2162d59
Astra <abhaypratapsinghwd@gmail.com>
2024-08-02 01:05:10 +0530

    Followers and Following Route

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,15 @@
 import User from "../models/User.js";
 
+// Give all the user info except the current User (req.user.id)
 export const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find({});
+    // Extract the current user's ID from the request object
+    const currentUserId = req.user.id;
+
+    // Fetch all users except the current user
+    const users = await User.find({ _id: { $ne: currentUserId } });
+
+    // Respond with the filtered list of users
     res.status(200).json({ users });
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error - Fetch Users" });
@@ -12,7 +19,7 @@ export const getAllUsers = async (req, res) => {
 export const getUserInfo = async (req, res) => {
   try {
     const userId = req.params.id;
-    const user = await User.findById(userId).populate("friends");
+    const user = await User.findById(userId);
 
     if (!user) {
       return res.status(404).json({ message: "User Not Found" });
@@ -21,6 +28,7 @@ export const getUserInfo = async (req, res) => {
     const { password, ...userInfo } = user._doc;
     res.status(200).json({ user: userInfo });
   } catch (err) {
+    console.log("err is ", err);
     res
       .status(500)
       .json({ message: "Internal Server Error - Fetch User Info" });
@@ -60,7 +68,8 @@ export const toggleFollow = async (req, res) => {
     await userToFollow.save();
 
     res.status(200).json({
-      user: currentUser,
+      currentUser,
+      userToFollow,
       message: isFollowing
         ? "UnFollowed successfully."
         : "Followed successfully.",
@@ -68,5 +77,61 @@ export const toggleFollow = async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Error toggling follow status." });
+  }
+};
+
+export const deleteAllUsers = async (req, res) => {
+  try {
+    const users = await User.deleteMany({});
+    res.json({ users, message: "Deleted All Users Successfully" });
+  } catch (error) {
+    console.log("Error deleting all users ", error);
+    return res
+      .status(500)
+      .json({ message: "Error deleting all users - Internal Server Error." });
+  }
+};
+
+export const getFollowers = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id).populate({
+      path: "followers",
+      select: "-password",
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ followers: user.followers });
+  } catch (error) {
+    console.log("Error fetching followers:", error);
+    res
+      .status(500)
+      .json({ message: "Internal Server error - fetching followers" });
+  }
+};
+
+export const getFollowing = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id).populate({
+      path: "following",
+      select: "-password",
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ following: user.following });
+  } catch (error) {
+    console.log("Error fetching following:", error);
+    res
+      .status(500)
+      .json({ message: "Internal Server error - fetching following" });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 import express from "express";
 import {
+  deleteAllUsers,
   getAllUsers,
+  getFollowers,
+  getFollowing,
   getUserInfo,
   toggleFollow,
 } from "../controllers/users.js";
@@ -11,10 +14,19 @@ const router = express.Router();
 // get all users when you are logged in
 router.get("/", authMiddleware, getAllUsers);
 
+//delete all users
+router.get("/delete-all-users", deleteAllUsers);
+
 // get user info of any user when you are logged in
 router.get("/:id", authMiddleware, getUserInfo);
 
 // toggleFollowing a user
-router.get("/follow/:userId", authMiddleware, toggleFollow);
+router.post("/follow/:userId", authMiddleware, toggleFollow);
+
+// get followers of a user
+router.get("/:id/followers", authMiddleware, getFollowers);
+
+// get following of a user
+router.get("/:id/following", authMiddleware, getFollowing);
 
 export default router;
