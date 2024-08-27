93b0810abbcafe8cbe5686b67d1cbe65b2b70e38
Astra <abhaypratapsinghwd@gmail.com>
2024-08-27 12:58:52 +0530

    Implemented Infinite Scroll in Posts Section

--- src/components/Profile/PostsSection.jsx
+++ src/components/Profile/PostsSection.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import PostsSectionSkeleton from "../../Skeleton/PostsSectionSkeleton";
 import EmptyPostsSection from "../../placeholders/Profile/EmptyPostsSection";
@@ -10,36 +10,61 @@ const PostsSection = () => {
   const { username } = useParams();
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const observerRef = useRef();
   const apiUrl = import.meta.env.VITE_API_URL;
 
+  const fetchUserPosts = async (page) => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `${apiUrl}/api/users/${username}/posts`,
+        { params: { page, limit: 10 }, withCredentials: true }
+      );
+      setPosts((prevPosts) => [...prevPosts, ...response.data.posts]);
+      if (page >= response.data.totalPages) {
+        setHasMore(false);
+      } else {
+        setHasMore(true);
+      }
+      console.log("response.data --fetchUserPosts is ", response.data);
+    } catch (err) {
+      console.log("error --fetchUserPosts is ", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const fetchUserPosts = async () => {
-      try {
-        // setLoading(true); ????
-        const response = await axios.get(
-          `${apiUrl}/api/users/${username}/posts`,
-          { withCredentials: true }
-        );
-        console.log("response.data --fetchUserPosts is ", response.data);
-        setPosts(response.data);
-      } catch (err) {
-        console.log("error --fetchUserPosts is ", err);
-        setError("Failed to fetch posts");
-      } finally {
-        setLoading(false);
+    fetchUserPosts(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasMore && !loading) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      },
+      { threshold: 1 }
+    );
+
+    const currentObserver = observerRef.current;
+    if (currentObserver) {
+      observer.observe(currentObserver);
+    }
+
+    return () => {
+      if (currentObserver) {
+        observer.unobserve(currentObserver);
       }
     };
+  }, [hasMore, loading]);
 
-    fetchUserPosts();
-  }, [apiUrl, username]);
-
-  if (loading) {
+  if (loading && page === 1) {
     return <PostsSectionSkeleton />;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
   }
 
   if (posts.length === 0) {
@@ -47,27 +72,36 @@ const PostsSection = () => {
   }
 
   return (
-    <div className="posts-grid">
-      {posts.map((post) => (
-        <Link to={`/posts/${post.slug}`} key={post._id} className="post-item">
-          {post.media[0].type === "image" ? (
-            <ProgressivePostImage
-              lowResUrl={post.media[0].lowResUrl}
-              highResUrl={post.media[0].highResUrl}
-              likes={post.likes.length}
-              comments={post.comments.length}
-              alt="Post media"
-            />
-          ) : (
-            <video
-              src={post.media[0].url}
-              className="post-thumbnail"
-              muted
-              loop
-            />
-          )}
-        </Link>
-      ))}
+    <div>
+      <div className="posts-grid">
+        {posts.map((post) => (
+          <Link to={`/posts/${post.slug}`} key={post._id} className="post-item">
+            {post.media[0].type === "image" ? (
+              <ProgressivePostImage
+                lowResUrl={post.media[0].lowResUrl}
+                highResUrl={post.media[0].highResUrl}
+                likes={post.likes.length}
+                comments={post.comments.length}
+                alt="Post media"
+              />
+            ) : (
+              <video
+                src={post.media[0].url}
+                className="post-thumbnail"
+                muted
+                loop
+              />
+            )}
+          </Link>
+        ))}
+      </div>
+      <div ref={observerRef} className="loading-more-posts">
+        {loading && (
+          <div className="spinner-container">
+            <div className="spinner"></div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
