47afa22ae6f9542eb948825f70208d9d8c7e5a03
Astra <abhaypratapsinghwd@gmail.com>
2024-08-31 23:09:39 +0530

    Access Chat on basis of username

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,27 +1,54 @@
 import Chat from "../models/Chat.js";
+import User from "../models/User.js";
 
 export const accessChat = async (req, res) => {
   try {
+    const { username } = req.params;
     const userId = req.user.id;
-    const { receiverId } = req.body;
+
+    const receiver = await User.findOne({ userName: username });
+    if (!receiver) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (userId === receiver._id) {
+      return res
+        .status(400)
+        .json({ message: "You cannot chat with yourself." });
+    }
 
     const chat = await Chat.findOne({
       isGroupChat: false,
-      participants: { $all: [userId, receiverId] },
+      participants: { $all: [userId, receiver._id] },
     }).populate("participants", "-password");
 
     if (chat) {
-      return res.json(chat);
+      return res.json({
+        chat,
+        receiver: {
+          userName: receiver.userName,
+          fullName: receiver.fullName,
+          profilePicture: receiver.profilePicture,
+        },
+      });
     }
 
     const newChat = new Chat({
-      participants: [userId, receiverId],
+      participants: [userId, receiver._id],
     });
 
     await newChat.save();
 
-    return res.status(201).json(newChat);
+    return res.status(201).json({
+      chat: newChat,
+      receiver: {
+        userName: receiver.userName,
+        fullName: receiver.fullName,
+        profilePicture: receiver.profilePicture,
+      },
+    });
   } catch (error) {
+    console.log("error.message is ", error.message);
     return res.status(500).json({ message: error.message });
   }
 };
--- routes/chat.js
+++ routes/chat.js
@@ -5,6 +5,6 @@ import authMiddleware from "../middleware/auth.js";
 const router = express.Router();
 
 // return one to one chat if it exists otherwise create a new one to one chat
-router.post("/", authMiddleware, accessChat);
+router.post("/@:username", authMiddleware, accessChat);
 
 export default router;
