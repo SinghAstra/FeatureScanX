1224ef8c0c3137be49e23c344804e66956846c0f
Astra <abhaypratapsinghwd@gmail.com>
2024-09-16 18:49:38 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({ credentials: true, origin: process.env.REMOTE }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
